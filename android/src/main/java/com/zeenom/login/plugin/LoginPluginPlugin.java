b3ccb4c92a10b2f55a7b89fbcccf131e934d06b8
zeeshantufail <zeeshan.tufail@aboitiz.com>
2025-04-29 11:01:35 +0800

    android login complete

--- android/src/main/java/com/zeenom/login/plugin/LoginPluginPlugin.java
+++ android/src/main/java/com/zeenom/login/plugin/LoginPluginPlugin.java
@@ -1,21 +1,23 @@
 package com.zeenom.login.plugin;
 
-import android.app.Activity;
+import static android.app.Activity.RESULT_OK;
+
 import android.content.Intent;
 import android.util.Log;
 
-import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.IntentSenderRequest;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
-import com.getcapacitor.annotation.ActivityCallback;
 import com.getcapacitor.annotation.CapacitorPlugin;
 import com.google.android.gms.auth.api.identity.BeginSignInRequest;
 import com.google.android.gms.auth.api.identity.Identity;
 import com.google.android.gms.auth.api.identity.SignInClient;
-import com.google.android.gms.auth.api.identity.SignInCredential;
 import com.google.android.gms.common.api.ApiException;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
@@ -23,156 +25,109 @@ import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.GoogleAuthProvider;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
 @CapacitorPlugin(name = "LoginPlugin")
 public class LoginPluginPlugin extends Plugin {
 
-    private LoginPlugin implementation = new LoginPlugin();
-
     private SignInClient oneTapClient;
     private BeginSignInRequest signInRequest;
+    private FirebaseAuth firebaseAuth;
 
-    @ActivityCallback
-    private void handleSignInResult(PluginCall call, ActivityResult result) {
-        Log.i("Zeeshan_debug", "handleSignInResult called: " + result);
-        Intent data = result.getData();
+    private ActivityResultLauncher<IntentSenderRequest> signInLauncher;
 
-        if (data == null) {
-            handleError(call, "No data returned from sign-in");
-            return;
-        }
-
-        try {
-            SignInCredential credential = oneTapClient.getSignInCredentialFromIntent(data);
-            String idToken = credential.getGoogleIdToken();
-
-            if (idToken != null) {
-                AuthCredential firebaseCredential = GoogleAuthProvider.getCredential(idToken, null);
-                FirebaseAuth.getInstance().signInWithCredential(firebaseCredential)
-                        .addOnCompleteListener(task -> {
-                            if (task.isSuccessful()) {
-                                FirebaseAuth.getInstance().getCurrentUser().getIdToken(true)
-                                        .addOnCompleteListener(tokenTask -> {
-                                            if (tokenTask.isSuccessful()) {
-                                                String firebaseToken = tokenTask.getResult().getToken();
-                                                JSObject response = new JSObject();
-                                                response.put("value", firebaseToken);
-                                                call.resolve(response);
-                                            } else {
-                                                Log.e("Zeeshan_debug LoginPlugin", "Failed to get Firebase token: " + tokenTask.getException().getMessage());
-                                                handleError(call, "Failed to get Firebase token: " + tokenTask.getException().getMessage());
-                                            }
-                                        });
-                            } else {
-                                Log.e("Zeeshan_debug LoginPlugin", "Firebase sign-in failed: " + task.getException().getMessage());
-                                handleError(call, "Firebase sign-in failed: " + task.getException().getMessage());
-                            }
-                        });
-            } else {
-                handleError(call, "No Google ID token received");
-            }
-
-        } catch (ApiException e) {
-            handleError(call, "Google sign-in failed: " + e.getMessage());
-        }
-    }
-
-    private void handleError(PluginCall call, String errorMessage) {
-        Log.e("Zeeshan_debug LoginPlugin", errorMessage);
-        JSObject errorResponse = new JSObject();
-        errorResponse.put("value", errorMessage);
-        call.resolve(errorResponse);
-    }
-
-    @PluginMethod
-    public void echo(PluginCall call) {
-        String value = call.getString("value", "");
-
-        Log.i("Zeeshan_debug", "Firebase config: " + value);
-
-        initializeFirebase(value, (res, error) -> {
-            Log.i("Zeeshan_debug", "Firebase initialized: " + res);
-            if (!res) {
-                JSObject response = new JSObject();
-                response.put("value", error);
-                call.resolve(response);
-                return;
-            }
-
-            Activity activity = getActivity();
-            oneTapClient = Identity.getSignInClient(activity);
-
-            signInRequest = new BeginSignInRequest.Builder()
-                    .setGoogleIdTokenRequestOptions(
-                            BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
-                                    .setSupported(true)
-                                    .setServerClientId("394556941374-r87tsj47981q4teq0mqvot913s44nrbp.apps.googleusercontent.com")
-                                    .setFilterByAuthorizedAccounts(false)
-                                    .build())
-                    .build();
-            Log.i("Zeeshan_debug", "Sign-in request created 2");
-            oneTapClient.beginSignIn(signInRequest)
-                    .addOnSuccessListener(activity, result -> {
-                        Log.i("Zeeshan_debug", "Sign-in request successful");
-                        try {
-                            startActivityForResult(call, activity.getIntent(), "handleSignInResult");
-                        } catch (Exception e) {
-                            Log.e("Zeeshan_debug", "Error starting intent: " + e.getMessage());
-                            JSObject response = new JSObject();
-                            response.put("value", "Google sign-in failed: " + e.getMessage());
-                            call.resolve(response);
+    @NonNull
+    private ActivityResultLauncher<IntentSenderRequest> signInLauncher() {
+        return getActivity().registerForActivityResult(new ActivityResultContracts.StartIntentSenderForResult(), result -> {
+            if (result.getResultCode() == RESULT_OK) {
+                Intent data = result.getData();
+                if (data != null) {
+                    try {
+                        com.google.android.gms.auth.api.identity.SignInCredential credential =
+                                oneTapClient.getSignInCredentialFromIntent(data);
+                        String idToken = credential.getGoogleIdToken();
+                        if (idToken != null) {
+                            AuthCredential firebaseCredential = GoogleAuthProvider.getCredential(idToken, null);
+                            firebaseAuth.signInWithCredential(firebaseCredential)
+                                    .addOnCompleteListener(task -> {
+                                        if (task.isSuccessful()) {
+                                            firebaseAuth.getCurrentUser().getIdToken(true)
+                                                    .addOnCompleteListener(tokenTask -> {
+                                                        if (tokenTask.isSuccessful()) {
+                                                            String finalIdToken = tokenTask.getResult().getToken();
+                                                            Log.d("MainActivity", "ID Token: " + finalIdToken);
+                                                            PluginCall call = bridge.getSavedCall(callId);
+                                                            call.resolve(new JSObject().put("value", finalIdToken));
+                                                            // Send the ID token to your backend
+                                                        } else {
+                                                            Log.e("MainActivity", "Failed to get final ID Token", tokenTask.getException());
+                                                        }
+                                                    });
+                                        } else {
+                                            Log.e("MainActivity", "Firebase Sign-in failed", task.getException());
+                                        }
+                                    });
+                        } else {
+                            Log.e("MainActivity", "No ID token!");
                         }
-                    })
-                    .addOnFailureListener(activity, e -> {
-                        Log.e("Zeeshan_debug", "Sign-in request failed: " + e.getMessage());
-                        JSObject response = new JSObject();
-                        response.put("value", "Google sign-in failed: " + e.getMessage());
-                        call.resolve(response);
-                    });
+                    } catch (ApiException e) {
+                        Log.e("MainActivity", "Sign-in failed: " + e.getLocalizedMessage());
+                    }
+                }
+            }
         });
     }
 
-    private void initializeFirebase(String jsonConfig, FirebaseInitCallback callback) {
-        try {
-            JSONObject config = new JSONObject(jsonConfig);
-
-//            String googleAppID = config.getString("GOOGLE_APP_ID");
-            String gcmSenderID = config.getString("GCM_SENDER_ID");
-            String apiKey = config.getString("API_KEY");
-            String clientID = config.getString("CLIENT_ID");
-            String projectID = config.getString("PROJECT_ID");
-            String bundleID = config.getString("BUNDLE_ID");
-            String storageBucket = config.optString("STORAGE_BUCKET", null);
-            String googleAppID = "1:336545645239:android:2a62452a6d30fdaf220332";
-//            String gcmSenderID = "394556941374";
-//            String apiKey = "AIzaSyCON86ECP_4ZoUzyK1IZLhHDcmQIB6Vuf0";
-//            String clientID = config.getString("CLIENT_ID");
-//            String projectID = "login-plugin-cc824";
-//            String bundleID = config.getString("BUNDLE_ID");
-//            String storageBucket = "login-plugin-cc824.firebasestorage.app";
-
-            FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setApplicationId(googleAppID)
-                    .setGcmSenderId(gcmSenderID)
-                    .setApiKey(apiKey)
-                    .setProjectId(projectID)
-                    .setStorageBucket(storageBucket)
-                    .build();
-
-            if (FirebaseApp.getApps(getContext()).isEmpty()) {
-                FirebaseApp.initializeApp(getContext(), options);
-            }
-
-            callback.onComplete(true, null);
-        } catch (JSONException e) {
-            Log.e("LoginPlugin", "Error parsing Firebase config: " + e.getMessage());
-            callback.onComplete(false, "Invalid Firebase config");
-        }
+    private void signInWithGoogle() {
+        oneTapClient.beginSignIn(signInRequest)
+                .addOnSuccessListener(result -> {
+                    try {
+                        IntentSenderRequest request = new IntentSenderRequest.Builder(result.getPendingIntent().getIntentSender()).build();
+                        signInLauncher.launch(request);
+                    } catch (Exception e) {
+                        Log.e("MainActivity", "Couldn't start One Tap UI: " + e.getLocalizedMessage());
+                    }
+                })
+                .addOnFailureListener(e -> Log.e("MainActivity", "One Tap sign-in failed: " + e.getLocalizedMessage()));
     }
 
-    private interface FirebaseInitCallback {
-        void onComplete(boolean result, String error);
+    @Override
+    public void load() {
+        super.load();
+        signInLauncher = signInLauncher();
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setProjectId("login-plugin-cc824")
+                .setApplicationId("1:394556941374:android:1ff959a91a2cc95c129d6f")
+                .setApiKey("AIzaSyCON86ECP_4ZoUzyK1IZLhHDcmQIB6Vuf0")
+                .build();
+
+        if (FirebaseApp.getApps(getActivity()).isEmpty()) {
+            FirebaseApp.initializeApp(getActivity(), options);
+        }
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        oneTapClient = Identity.getSignInClient(getActivity());
+
+        signInRequest = BeginSignInRequest.builder()
+                .setGoogleIdTokenRequestOptions(
+                        BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                                .setSupported(true)
+                                .setServerClientId("394556941374-7t8udgrmmp21og7dhi7vqtqp920dop4g.apps.googleusercontent.com")
+                                .setFilterByAuthorizedAccounts(false)
+                                .build()
+                )
+                .setAutoSelectEnabled(false)
+                .build();
+    }
+
+    private String callId;
+
+    @PluginMethod
+    public void echo(PluginCall call) {
+
+        Log.i("Zeeshan_debug", "Echo called: " + call.getString("value"));
+
+        bridge.saveCall(call);
+        callId = call.getCallbackId();
+        signInWithGoogle();
     }
 }
